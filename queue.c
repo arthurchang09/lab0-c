7f339f2920901cc94360f796161f10b85faef4ad
arthurchang09 <arthurchang09@gmail.com>
2022-02-18 11:46:43 +0800

    Work on q_remove_rail for constant time Use memcpy() instead of strcpy() to see if it works on constant time.

--- queue.c
+++ queue.c
@@ -146,7 +146,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     // cppcheck-suppress nullPointer
     element_t *rm = list_entry(head->prev, element_t, list);
     if (sp) {
-        strncpy(sp, rm->value, bufsize - 1);
+        memcpy(sp, rm->value, bufsize);
         sp[bufsize - 1] = '\0';
     }
     rm->list.prev->next = head;
